d03fc1bc7972ac6ec1e9246670e571170f239261
happysir <552446860@qq.com>
2019-03-12 23:04:36 +0800

    2019年3月12日23:04:35

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import YMNode from './lib/YM/YMNode'
-import { rightCenter } from './lib/Layout/ruleLayoutStrategy'
+import { rightCenter, rightBottom, rightTop } from './lib/Layout/ruleLayoutStrategy'
 import { columnStrategy } from './lib/Layout/childLayoutStrategy'
 import { rootRender } from './lib/Layout/ruleLayoutStrategy'
 
@@ -8,20 +8,30 @@ const root = new YMNode('root');
 const n1 = new YMNode('n001')
 const n2 = new YMNode('n002')
 const n3 = new YMNode('n003')
+const n11 = new YMNode('n0011')
+const n12 = new YMNode('n0012')
+
 root.children = [n1, n2, n3]
 n1.parent = root
 n2.parent = root
 n3.parent = root
-const nodes = [root, n1, n2, n3]
+n1.children = [n11, n12]
+n11.parent = n1
+n12.parent = n1
+const nodes = [root, n1, n11, n12, n2, n3]
 
 // 注入绘制策略
 root.ruleArea.strategy = rightCenter
 root.childrenArea.strategy = columnStrategy
 
+n1.ruleArea.strategy = rightCenter
+n1.childrenArea.strategy = columnStrategy
 
 function test() {
     root.calcRuleAreaWH()
     rootRender(root) // 临时
+    root.container.style.left = `${root.selfArea.start.x}px`
+    root.container.style.top = `${root.selfArea.start.y}px`
     for (var i = 1; i < nodes.length; i++) {
         var node = nodes[i]
         node.selfArea.start.x = node.parent.selfArea.start.x
@@ -30,18 +40,20 @@ function test() {
         node.selfArea.start.y = node.parent.selfArea.start.y
             + node.parent.childrenArea.start.y
             + node.ruleArea.start.y
-    }
-
-    for (var i = 0; i < nodes.length;i++) {
-        var node = nodes[i]
         node.container.style.left = `${node.selfArea.start.x}px`
         node.container.style.top = `${node.selfArea.start.y}px`
     }
-
     console.log(root)
+    rootRuleArea.style.outline = `1px solid red`
+    rootRuleArea.style.width = `${root.ruleArea.width}px`
+    rootRuleArea.style.height = `${root.ruleArea.height}px`
+    rootRuleArea.style.left = `${root.ruleArea.start.x}px`
+    rootRuleArea.style.top = `${root.ruleArea.start.y}px`
 }
+
+window.root = root
+let rootRuleArea = document.getElementById('rootRuleArea')
+
 
 let button = document.getElementById('submit')
 button.onclick = test
-
-window.root = root
